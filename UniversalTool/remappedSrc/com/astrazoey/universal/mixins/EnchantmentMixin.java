b726ece965a957fcbfa6d929bf46df669b464c91
Zoey <jragon.alt1@gmail.com>
2025-10-22 17:02:50 +0800

    moved the mattock to its own repo

--- UniversalTool/remappedSrc/com/astrazoey/universal/mixins/EnchantmentMixin.java
+++
@@ -1,8 +0,0 @@
-package com.astrazoey.universal.mixins;
-
-import net.minecraft.enchantment.Enchantment;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Enchantment.class)
-public class EnchantmentMixin {
-}
